bbc0b070ee788124c6de99a990802d5745b7e5f7
Rolan Robinson <rolan83@hotmail.com>
2021-04-11 13:23:01 -0500

    Fix index on all for department, Qualification, RelationshipType and also user auth login

--- HR-Management-System/SeedData.cs
+++ HR-Management-System/SeedData.cs
@@ -17,24 +17,24 @@ namespace HR_Management_System
 
         private static void SeedUsers(UserManager<IdentityUser> userManager)
         {
-            if(userManager.FindByNameAsync("admin").Result == null)
+            if(userManager.FindByNameAsync("admin@localhost").Result == null)
             {
                 var user = new IdentityUser
                 {
-                    UserName = "admin",
-                    Email = "admin@localhoust"
+                    UserName = "admin@localhost.com",
+                    Email = "admin@localhost.com"
                 };
                 var results = userManager.CreateAsync(user, "P@ssword1").Result;
                 if (results.Succeeded)
                 {
-                    userManager.AddToRoleAsync(user, "Aministrator").Wait();
+                    userManager.AddToRoleAsync(user, "Administrator").Wait();
                 }
             }
         }
 
         private static void SeedRoles(RoleManager<IdentityRole> roleManager)
         {
-            if (roleManager.RoleExistsAsync("Administrator").Result)
+            if (!roleManager.RoleExistsAsync("Administrator").Result)
             {
                 var role = new IdentityRole
                 {
@@ -43,7 +43,7 @@ namespace HR_Management_System
                 var result = roleManager.CreateAsync(role).Result;
             }
 
-            if (roleManager.RoleExistsAsync("Employee").Result)
+            if (!roleManager.RoleExistsAsync("Employee").Result)
             {
                 var role = new IdentityRole
                 {
